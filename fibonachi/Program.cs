0e73aee739a57f78f534c44b67471f85f4ad294c
Mohammad Rezazadeh <mohammad1382144@gmail.com>
2023-06-09 22:42:44 +0330

    fibonachi

---
+++ fibonachi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace fibonachi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10; // تعداد جملات در دنباله فیبوناچی
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", Fibonacci(i));
+            }
+        }
+
+        static int Fibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
